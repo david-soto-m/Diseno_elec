1a74c8e818499cd1ccb9c7523ed8a2a641d18714
david-soto-m <61031415+david-soto-m@users.noreply.github.com>
2020-11-08 13:11:58 +0100

    Comentaro
    Respuesta al comentaroio

--- main.py
+++ main.py
@@ -7,4 +7,5 @@ if __name__=="__main__":
 # No conocia esta forma de hacer lo que has hecho
 # Con lo que yo he aprendido simplemente hubiera puesto: main()
 # Lo de __main__, ¿qué es una especie de main predefinido de python??
+# Sip, sirve para que puedas hacer cosas distintas dependiendo de si importas al paquete o lo llamas directamente para ejecución
 
