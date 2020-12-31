2b94326c3edacf7d69bf08da3b998f6155a015f2
david-soto-m <davidsotomarchena@protonmail.com>
2020-12-31 18:16:09 +0100

    Trabajadores

--- xmanage/registros.py
+++ xmanage/registros.py
@@ -1,1 +1,6 @@
- 
+#!/usr/bin/python3
+
+import xml.etree.ElementTree as ET
+
+tree = ET.parse('.xml')
+root = tree.getroot()
--- xmanage/trabajadores.py
+++ xmanage/trabajadores.py
@@ -1,1 +1,17 @@
- 
+#!/usr/bin/python3
+
+import xml.etree.ElementTree as ET
+
+
+tree = ET.parse("../xmls/trabajadores.xml")
+root = tree.getroot()
+	
+def es_trabajador(cadena):
+	elemento=root.find("./Trabajador[@codigo='"+cadena+"']")
+	if elemento is None:
+		return(False,"")
+	else:
+		return(True,elemento.text)
+
+if __name__=="__main__":
+	print(es_trabajador("00001"))
