599ab97b0ed83f10f147c72ccc891e544911478d
david-soto-m <davidsotomarchena@protonmail.com>
2020-12-31 14:08:28 +0100

    html to php

--- web/apertura.php
+++ web/apertura.php
@@ -1,29 +1,25 @@
- <html>
-	<head>
-		<meta charset="utf-8" />
-		<title>Apertura de Puerta</title>
-		<style type="text/css">
-			code{white-space: pre-wrap;}
-			span.smallcaps{font-variant: small-caps;}
-			span.underline{text-decoration: underline;}
-			div.column{display: inline-block; vertical-align: top; width: 50%;}
-		</style>
-		<link rel="stylesheet" href="web.css"/>
-	</head>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <title>Apertura de Puerta</title>
+  <style type="text/css">
+      code{white-space: pre-wrap;}
+      span.smallcaps{font-variant: small-caps;}
+      span.underline{text-decoration: underline;}
+      div.column{display: inline-block; vertical-align: top; width: 50%;}
+  </style>
+  <link rel="stylesheet" href="web.css"/>
+</head>
+<body>
+	<h1>¿Tiene esta persona permiso para entrar?</h1>
 	<?php
-//	echo "Empezando...";
-//	print $_REQUEST[Estado];
-	if ($_REQUEST[Estado]=="Si"){
-		echo "Dejando Pasar";
-// 		exec("sudo python3 abre.py");
-		}
-	if ($_REQUEST[Estado]=="No"){
-		echo "Bloqueando";
-// 		exec("sudo python3 led_off.py");
-//		echo "Se ha apagado el led";
-		}
-	echo "<br/>";
-        ?>
-
-<a href="cambia_led.html"> Volver </a>
+		$a=exec("sudo python3 FotoPython.py")
+	?>
+	<img src="foto.jpg" alt="Foto" />
+	</br>
+	<form action="apertura_recibido.php" method="get">
+		<input type="submit" name="Estado" value="Si"/>
+		<input type="submit" name="Estado" autofocus=true value="No"/>
+	</form>
+</body>
 </html>
---
+++ web/apertura_recibido.php
@@ -0,0 +1,29 @@
+ <html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Apertura de Puerta</title>
+		<style type="text/css">
+			code{white-space: pre-wrap;}
+			span.smallcaps{font-variant: small-caps;}
+			span.underline{text-decoration: underline;}
+			div.column{display: inline-block; vertical-align: top; width: 50%;}
+		</style>
+		<link rel="stylesheet" href="web.css"/>
+	</head>
+	<?php
+//	echo "Empezando...";
+//	print $_REQUEST[Estado];
+	if ($_REQUEST[Estado]=="Si"){
+		echo "Dejando Pasar";
+// 		exec("sudo python3 abre.py");
+		}
+	if ($_REQUEST[Estado]=="No"){
+		echo "Bloqueando";
+// 		exec("sudo python3 led_off.py");
+//		echo "Se ha apagado el led";
+		}
+	echo "<br/>";
+        ?>
+
+<a href="apertura.php"> Volver </a>
+</html>
