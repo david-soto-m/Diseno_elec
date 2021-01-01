c53849537a3aa3ab5ddc301b7d786eb4975fda41
Marcelo <marcelojv36@gmail.com>
2021-01-01 15:16:56 +0100

    aportacion web3 marcelo

---
+++ xmanage/incorporaciones.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import xml.etree.ElementTree as ET
+
+
+def comprueba_valido(codigo):
+	respuesta = True
+
+	if len(nombre)!=5:
+		respuesta=False
+	else:
+		try:
+			a = int(codigo)
+		except:
+			respuesta = False
+
+	if respuesta==True:
+		tree = ET.parse("xmls/trabajadores.xml")
+		root = tree.getroot()
+		elemento=root.find("./Trabajador[@codigo='"+codigo+"']")
+		if elemento is not None:
+			respuesta = False
+			
+	return respuesta
+
+
+
+def nuevo_registro(codigo,nombre):
+	#leer archivo
+	archivo="xmls/trabajadores.xml"
+	tree = ET.parse(archivo)
+	root = tree.getroot()
+	#Crear registro
+	entrada=ET.SubElement(root,'Trabajador')
+	entrada.set("codigo",codigo)
+
+# FALTA: decir que "nombre" es el "text" de la entrada
+
+
+#	Fecha=ET.SubElement(entrada,"Fecha")
+#	Fecha.text=FechaStr
+
+	#Reescribir archivo
+	tree.write(archivo)
