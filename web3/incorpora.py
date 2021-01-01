b951b14a2d4f5adfcfb8de191a95beb222f523e5
david-soto-m <davidsotomarchena@protonmail.com>
2021-01-01 20:47:00 +0100

    prueba ejecuta python 3

--- web3/incorpora.py
+++ web3/incorpora.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 
-import incorporaciones as I 
-from twython import Twython
-import sys
+#import incorporaciones as I 
+#from twython import Twython
+#import sys
 
-#########################################################
-def publica_en_twitter(nombre):
-	# Twitter: @RpiMarcelo
-	APP_KEY = "wvbENKZWzWGbBnV1O93z9BaKW"
-	APP_SECRET = "qK76rTuHGwTkWukZvt1ce6zVwpI0yoKxDz1qQBgSXpGaHKiHac"
-	OAUTH_TOKEN = "1334992616853794817-CmrY3VgiifqRItNQAMFhQCEEFYROY5"
-	OAUTH_TOKEN_SECRET = "VSIDNE824r14dpNQ5Hsa8lQfdP1xH9BhCOxc6AwQpxiyd"
-	twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-	Texto = nombre + " se ha incorporado a nuestra empresa. ¡Bienvenido!"
-	# Publicación del tweet
-	twitter.update_status(status=Texto)
+#################################################
+#def publica_en_twitter(nombre):
+	#Twitter: @RpiMarcelo
+	#APP_KEY = "wvbENKZWzWGbBnV1O93z9BaKW"
+	#APP_SECRET = "qK76rTuHGwTkWukZvt1ce6zVwpI0yoKxDz1qQBgSXpGaHKiHac"
+	#OAUTH_TOKEN = "1334992616853794817-CmrY3VgiifqRItNQAMFhQCEEFYROY5"
+	#OAUTH_TOKEN_SECRET = "VSIDNE824r14dpNQ5Hsa8lQfdP1xH9BhCOxc6AwQpxiyd"
+	#twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+	#Texto = nombre + " se ha incorporado a nuestra empresa. ¡Bienvenido!"
+	#Publicación del tweet
+	#twitter.update_status(status=Texto)
 #########################################################
 # Lectura de la informacion de publicacion del trabajador
 
