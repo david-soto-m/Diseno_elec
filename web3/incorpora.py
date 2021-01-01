ea3723ee7018ee404321f0b8ec038950e1521ce5
david-soto-m <davidsotomarchena@protonmail.com>
2021-01-01 21:16:36 +0100

    hasta un tonto avanza si se choca con todo

--- web3/incorpora.py
+++ web3/incorpora.py
@@ -5,16 +5,16 @@
 import sys
 
 #################################################
-#def publica_en_twitter(nombre):
-	#Twitter: @RpiMarcelo
-	#APP_KEY = "wvbENKZWzWGbBnV1O93z9BaKW"
-	#APP_SECRET = "qK76rTuHGwTkWukZvt1ce6zVwpI0yoKxDz1qQBgSXpGaHKiHac"
-	#OAUTH_TOKEN = "1334992616853794817-CmrY3VgiifqRItNQAMFhQCEEFYROY5"
-	#OAUTH_TOKEN_SECRET = "VSIDNE824r14dpNQ5Hsa8lQfdP1xH9BhCOxc6AwQpxiyd"
-	#twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-	#Texto = nombre + " se ha incorporado a nuestra empresa. ¡Bienvenido!"
-	#Publicación del tweet
-	#twitter.update_status(status=Texto)
+def publica_en_twitter(nombre):
+	Twitter: @RpiMarcelo
+	APP_KEY = "wvbENKZWzWGbBnV1O93z9BaKW"
+	APP_SECRET = "qK76rTuHGwTkWukZvt1ce6zVwpI0yoKxDz1qQBgSXpGaHKiHac"
+	OAUTH_TOKEN = "1334992616853794817-CmrY3VgiifqRItNQAMFhQCEEFYROY5"
+	OAUTH_TOKEN_SECRET = "VSIDNE824r14dpNQ5Hsa8lQfdP1xH9BhCOxc6AwQpxiyd"
+	twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+	Texto = nombre + " se ha incorporado a nuestra empresa. ¡Bienvenido!"
+	Publicación del tweet
+	twitter.update_status(status=Texto)
 #########################################################
 # Lectura de la informacion de publicacion del trabajador
 
@@ -22,13 +22,12 @@ reparto=" ".join(sys.argv[1:]).split("-*-")
 codigo = reparto[0]
 nombre = reparto[1]
 publicar = reparto[2] =="0"
-print(codigo,nombre,publicar)
-#respuesta = I.comprueba_valido(codigo)
+respuesta = I.comprueba_valido(codigo)
 
-#if respuesta:
-	#I.nuevo_registro(codigo,nombre)
-	
-	#if publicar=="Si":
-		#publica_en_twitter(nombre)
-#else:
-	#print("Invalido")
+if respuesta:
+	I.nuevo_registro(codigo,nombre)
+	print("valido")
+	if publicar=="Si":
+		publica_en_twitter(nombre)
+else:
+	print("Invalido")
